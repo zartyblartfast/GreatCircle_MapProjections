b2355b486ebf6b56639e3a657a8c3763dc9a9ce4
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-13 14:57:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
-from flask import Flask, render_template, request
-import your_script_name  # Import your script here.
+from flask import Flask, render_template, request, send_file
+from map_generator import main as generate_map
+import cartopy.crs as ccrs
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        projection_type = request.form.get('projectionType')  # Replace 'projectionType' with the actual name of the form input for the map projection.
-        location1_name = request.form.get('location1Name')
-        location1_lat = request.form.get('location1Lat')
-        location1_long = request.form.get('location1Long')
-        # Repeat for location 2.
+        # Get user input
+        location1 = request.form.get('location1').split(',')
+        location2 = request.form.get('location2').split(',')
+        location3 = request.form.get('location3').split(',')
+        location4 = request.form.get('location4').split(',')
+        projection = request.form.get('projection')
+        
+        # Process user input
+        locations = [
+            (location1[0], float(location1[1]), float(location1[2])),
+            (location2[0], float(location2[1]), float(location2[2])),
+            (location3[0], float(location3[1]), float(location3[2])),
+            (location4[0], float(location4[1]), float(location4[2])),
+        ]
+        if projection == 'PlateCarree':
+            projection = ccrs.PlateCarree()
+        elif projection == 'AzimuthalEquidistant':
+            projection = ccrs.AzimuthalEquidistant(central_latitude=90, central_longitude=0)
+        
+        # Generate the map
+        generate_map(projection, locations)
+        
+        # Serve the generated map to the user
+        return send_file('map_image.png', mimetype='image/png')
 
-        # Use these variables as arguments for a function in your script that generates and saves the map image.
-        your_script_name.main(projection_type, location1_name, location1_lat, location1_long)  # Replace 'main' with the actual name of the function.
+    return render_template('index.html')
 
-        return 'Map image generated.'  # Replace this with code to display the image on the webpage.
-
-    else:
-        return render_template('index.html')
+if __name__ == "__main__":
+    app.run(debug=True)
