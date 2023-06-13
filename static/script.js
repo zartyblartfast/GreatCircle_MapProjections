03101e00c0bc6952c09958c85ba60852cfa380ee
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-13 17:00:38 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,12 +2,3 @@ document.getElementById('toggleSecondPair').addEventListener('change', function(
     document.getElementById('secondPair').hidden = !this.checked;
 });
 
-table {
-    width: 100%;
-    margin-bottom: 20px;
-}
-
-table th, table td {
-    padding: 10px;
-    text-align: left;
-}
