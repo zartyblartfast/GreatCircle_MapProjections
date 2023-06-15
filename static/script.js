013da8f224cac3fbeb87c319b21433ae4f6431d7
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-15 09:08:45 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,11 +3,21 @@ document.getElementById('locationsForm').addEventListener('submit', function() {
     this.querySelector('[type="submit"]').disabled = true;
 });
 
-document.getElementById('toggleSecondPair').addEventListener('click', function() {
+document.getElementById('includeSecondPair').addEventListener('change', function() {
     var secondPair = document.getElementById('secondPair');
-    if (secondPair.hidden) {
-        secondPair.hidden = false;
+    var inputs = secondPair.querySelectorAll('input');
+    if (this.checked) {
+        // Enable all inputs in second pair
+        inputs.forEach(function(input) {
+            input.disabled = false;
+        });
     } else {
-        secondPair.hidden = true;
+        // Disable all inputs in second pair
+        inputs.forEach(function(input) {
+            input.disabled = true;
+        });
     }
 });
+
+// Trigger the change event at page load to apply correct state
+document.getElementById('includeSecondPair').dispatchEvent(new Event('change'));
