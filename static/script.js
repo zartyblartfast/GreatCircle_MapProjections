efcd6e2b2a7464e97c27e5b3c2a7b9c2437d0216
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-15 14:43:47 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,42 +1,40 @@
-var spinner = document.getElementById('spinner');
+window.addEventListener('load', function() {
+    var spinner = document.getElementById('spinner');
+    spinner.style.display = "none"; // Hide the spinner initially
 
-document.getElementById('locationsForm').addEventListener('submit', function(e) {
-    document.getElementById('spinner').style.display = 'inline';
-    if(!document.getElementById('includeSecondPair').checked) {
-        document.getElementById('secondPair').style.display = 'none';
-    }
-});
+    document.getElementById('locationsForm').addEventListener('submit', function(e) {
+        spinner.style.display = 'inline';
+        if(!document.getElementById('includeSecondPair').checked) {
+            document.getElementById('secondPair').style.display = 'none';
+        }
+    });
 
-document.getElementById('includeSecondPair').addEventListener('change', function() {
-    var secondPair = document.getElementById('secondPair');
-    if (this.checked) {
-        // Show second pair and enable all inputs
-        secondPair.style.display = "";
-        var inputs = secondPair.querySelectorAll('input');
-        inputs.forEach(function(input) {
-            input.disabled = false;
-        });
-        // Save checkbox state
-        localStorage.setItem('includeSecondPair', 'checked');
-    } else {
-        // Hide second pair and disable all inputs
-        secondPair.style.display = "none";
-        var inputs = secondPair.querySelectorAll('input');
-        inputs.forEach(function(input) {
-            input.disabled = true;
-        });
-        // Save checkbox state
-        localStorage.setItem('includeSecondPair', '');
-    }
-});
+    document.getElementById('includeSecondPair').addEventListener('change', function() {
+        var secondPair = document.getElementById('secondPair');
+        if (this.checked) {
+            // Show second pair and enable all inputs
+            secondPair.style.display = "";
+            var inputs = secondPair.querySelectorAll('input');
+            inputs.forEach(function(input) {
+                input.disabled = false;
+            });
+            // Save checkbox state
+            localStorage.setItem('includeSecondPair', 'checked');
+        } else {
+            // Hide second pair and disable all inputs
+            secondPair.style.display = "none";
+            var inputs = secondPair.querySelectorAll('input');
+            inputs.forEach(function(input) {
+                input.disabled = true;
+            });
+            // Save checkbox state
+            localStorage.setItem('includeSecondPair', '');
+        }
+    });
 
-// Retrieve checkbox state and trigger the change event at page load to apply correct state
-window.onload = function() {
+    // Retrieve checkbox state and trigger the change event at page load to apply correct state
     var isChecked = localStorage.getItem('includeSecondPair');
     var checkbox = document.getElementById('includeSecondPair');
     checkbox.checked = (isChecked === 'checked') ? true : false;
     checkbox.dispatchEvent(new Event('change'));
-
-    // Hide the spinner initially
-    spinner.style.display = "none";
-};
+});
