400ac19a8172fda805a592699c09d182ed64f0a0
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-15 07:46:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -81,12 +81,14 @@ def index():
 
 @app.route('/map1/<filename>', methods=['GET'])
 def serve_map1(filename):
-    image_path = os.path.join('/home/zartyblartfast/GreatCircle_MapProjections', filename)
+    #image_path = os.path.join('/home/zartyblartfast/GreatCircle_MapProjections', filename)
+    image_path = os.path.join('/home/zartyblartfast/', filename)
     return send_file(image_path, mimetype='image/png')
 
 @app.route('/map2/<filename>', methods=['GET'])
 def serve_map2(filename):
-    image_path = os.path.join('/home/zartyblartfast/GreatCircle_MapProjections', filename)
+    #image_path = os.path.join('/home/zartyblartfast/GreatCircle_MapProjections', filename)
+    image_path = os.path.join('/home/zartyblartfast/', filename)
     return send_file(image_path, mimetype='image/png')
 
 if __name__ == "__main__":
