6eaa900243af42f6e01d391c3ae72d05ef503c23
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-15 22:02:12 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -10,11 +10,31 @@ window.addEventListener('load', function() {
 
     document.getElementById('locationsForm').addEventListener('submit', function(e) {
         spinner.style.display = 'inline';
-        if(!includeSecondPair.checked) {
-            secondPair.style.display = 'none';
-        }
     });
 
     includeSecondPair.addEventListener('change', function() {
         if (this.checked) {
             // Show second pair and enable all inputs
+            secondPair.style.display = "";
+            var inputs = secondPair.querySelectorAll('input');
+            inputs.forEach(function(input) {
+                input.disabled = false;
+            });
+            // Save checkbox state
+            localStorage.setItem('includeSecondPair', 'checked');
+        } else {
+            // Hide second pair and disable all inputs
+            secondPair.style.display = "none";
+            var inputs = secondPair.querySelectorAll('input');
+            inputs.forEach(function(input) {
+                input.disabled = true;
+            });
+            // Save checkbox state
+            localStorage.setItem('includeSecondPair', '');
+        }
+    });
+
+    // Retrieve checkbox state from local storage and update checkbox state
+    var isChecked = localStorage.getItem('includeSecondPair');
+    includeSecondPair.checked = (isChecked === 'checked') ? true : false;
+});
