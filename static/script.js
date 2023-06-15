34700b7618ea83909ea66d33c8708329b373bb33
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-15 08:45:23 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,8 @@
+document.getElementById('locationsForm').addEventListener('submit', function() {
+    // Disable the submit button
+    this.querySelector('[type="submit"]').disabled = true;
+});
+
 document.getElementById('toggleSecondPair').addEventListener('click', function() {
     var secondPair = document.getElementById('secondPair');
     if (secondPair.hidden) {
