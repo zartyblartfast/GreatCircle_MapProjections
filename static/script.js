766dfff490230c99cc3eebad681e5897696c7b00
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-15 23:04:29 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -4,21 +4,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var includeSecondPair = document.getElementById('includeSecondPair');
 
     spinner.style.display = "none"; // Hide the spinner initially
+    includeSecondPair.checked = true; // Check the checkbox initially
 
-    // Set the initial checkbox and second pair states.
-    var isChecked = localStorage.getItem('includeSecondPair');
-    if (isChecked === null) { // First visit
-        includeSecondPair.checked = true;
-        localStorage.setItem('includeSecondPair', 'checked');
-    } else { // Returning visit
-        includeSecondPair.checked = isChecked === 'checked';
-    }
-    updateSecondPairState();
+    // Always show the second pair on the initial load
+    secondPair.style.display = "table";
 
-    // When checkbox state changes, update the second pair state.
-    includeSecondPair.addEventListener('change', function() {
-        localStorage.setItem('includeSecondPair', this.checked ? 'checked' : '');
-        updateSecondPairState();
+    document.getElementById('locationsForm').addEventListener('submit', function(e) {
+        spinner.style.display = 'inline';
+        if (!includeSecondPair.checked) {
+            secondPair.style.display = 'none';
+        }
     });
 
-    document.getElementById
+    includeSecondPair.addEventListener('change', function() {
+        if (this.checked) {
+            // Show second pair and enable all inputs
+            secondPair.style.display = "table";
+            var inputs = secondPair.querySelectorAll('input');
+            inputs.forEach(function(input) {
+                input.disabled = false;
+            });
+        } else {
+            // Hide second pair and disable all inputs
+            secondPair.style.display = "none";
+            var inputs = secondPair.querySelectorAll('input');
+            inputs.forEach(function(input) {
+                input.disabled = true;
+            });
+        }
+    });
+});
