79b0b43fe3318b5608508ee0658c32e63a305ea2
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-15 14:00:29 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,11 @@
-document.getElementById('locationsForm').addEventListener('submit', function() {
+var spinner = document.getElementById('spinner');
+
+document.getElementById('locationsForm').addEventListener('submit', function(event) {
     // Disable the submit button
     this.querySelector('[type="submit"]').disabled = true;
+
+    // Show the spinner
+    spinner.style.display = "";
 });
 
 document.getElementById('includeSecondPair').addEventListener('change', function() {
@@ -32,4 +37,7 @@ window.onload = function() {
     var checkbox = document.getElementById('includeSecondPair');
     checkbox.checked = (isChecked === 'checked') ? true : false;
     checkbox.dispatchEvent(new Event('change'));
+
+    // Hide the spinner initially
+    spinner.style.display = "none";
 };
