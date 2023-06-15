7b02f99adec061a066106fc248ca4eabdaf94965
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-15 22:55:44 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -5,9 +5,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     spinner.style.display = "none"; // Hide the spinner initially
 
-    // Always show the second pair and check the checkbox on initial load
-    secondPair.style.display = "";
-    includeSecondPair.checked = true;
+    // Retrieve checkbox state from local storage and update checkbox state
+    var isChecked = localStorage.getItem('includeSecondPair');
+    if (isChecked === null) { // First visit
+        includeSecondPair.checked = true;
+        localStorage.setItem('includeSecondPair', 'checked');
+    } else { // Returning visit
+        includeSecondPair.checked = (isChecked === 'checked') ? true : false;
+    }
+
+    // Apply checkbox state to the visibility of the second pair
+    secondPair.style.display = includeSecondPair.checked ? "" : "none";
+    var inputs = secondPair.querySelectorAll('input');
+    inputs.forEach(function(input) {
+        input.disabled = !includeSecondPair.checked;
+    });
 
     includeSecondPair.addEventListener('change', function() {
         if (this.checked) {
@@ -17,6 +29,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
             inputs.forEach(function(input) {
                 input.disabled = false;
             });
+            // Save checkbox state
+            localStorage.setItem('includeSecondPair', 'checked');
         } else {
             // Hide second pair and disable all inputs
             secondPair.style.display = "none";
@@ -24,10 +38,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
             inputs.forEach(function(input) {
                 input.disabled = true;
             });
+            // Save checkbox state
+            localStorage.setItem('includeSecondPair', '');
         }
     });
 
     document.getElementById('locationsForm').addEventListener('submit', function(e) {
         spinner.style.display = 'inline';
-    });
-});
