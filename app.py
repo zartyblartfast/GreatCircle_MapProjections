589b75ec11ad4415f8f8a492a89037a22977ec64
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-16 23:42:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,52 +23,49 @@ def convert_coord(coord_str):
         return None
     return coord
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # Define the variables with default values here
     filename_plate_carree = "default_PlateCarree.png"
     filename_azimuthal_equidistant = "default_AzimuthalEquidistant.png"
-    location1_str = ["", "", ""]
-    location2_str = ["", "", ""]
-    location3_str = ["", "", ""]
-    location4_str = ["", "", ""]
-    plot_second_pair = False  # Set default as False
 
-    logging.info(f"request.method: {request.method}")
-    
+    location1_str = ["", "0", "0"]
+    location2_str = ["", "0", "0"]
+    location3_str = ["", "0", "0"]
+    location4_str = ["", "0", "0"]
+
+    plot_second_pair = False
+
     if request.method == 'POST':
         logging.info('Received POST request')
 
         location1_str = [request.form.get('location1Name'), request.form.get('location1Lat'), request.form.get('location1Lon')]
         location2_str = [request.form.get('location2Name'), request.form.get('location2Lat'), request.form.get('location2Lon')]
-        plot_second_pair = 'plotSecondPair' in request.form  # Fetch checkbox status and convert it to Boolean
-        logging.info(f"plot_second_pair: {plot_second_pair}")
+        plot_second_pair = 'plotSecondPair' in request.form
         
         location1 = [location1_str[0], convert_coord(location1_str[1]), convert_coord(location1_str[2])]
         location2 = [location2_str[0], convert_coord(location2_str[1]), convert_coord(location2_str[2])]
 
-        if None in location1 or None in location2:
-            logging.info("Location 1 or Location 2 were not provided in correct format")
+        if None in location1 or None in location2 or "" in location1_str or "" in location2_str:
+            logging.error("Location 1 or Location 2 were not provided in correct format")
             return "Location 1 or Location 2 were not provided in correct format"
 
         locations = [tuple(location1), tuple(location2)]
-        logging.info(f"locations: {locations}")
-        
-        # Only fetch and plot the second pair of locations if checkbox is enabled
-        if plot_second_pair == 'on':  # New condition for checkbox
+
+        if plot_second_pair:
             location3_str = [request.form.get('location3Name'), request.form.get('location3Lat'), request.form.get('location3Lon')]
             location4_str = [request.form.get('location4Name'), request.form.get('location4Lat'), request.form.get('location4Lon')]
             
-            # Check for non-empty longitude as well for locations 3 and 4
-            if location3_str[0] and location3_str[1] and location3_str[2] and location4_str[0] and location4_str[1] and location4_str[2]:
-                location3 = [location3_str[0], convert_coord(location3_str[1]), convert_coord(location3_str[2])]
-                location4 = [location4_str[0], convert_coord(location4_str[1]), convert_coord(location4_str[2])]
-                locations.extend([tuple(location3), tuple(location4)])
-            else:
-                logging.error("empty longitude as well as locations 3 and 4")
+            location3 = [location3_str[0], convert_coord(location3_str[1]), convert_coord(location3_str[2])]
+            location4 = [location4_str[0], convert_coord(location4_str[1]), convert_coord(location4_str[2])]
 
-        logging.info(f"Locations: {locations}")
+            if None in location3 or None in location4 or "" in location3_str or "" in location4_str:
+                logging.error("Location 3 or Location 4 were not provided in correct format")
+                return "Location 3 or Location 4 were not provided in correct format"
+            
+            locations.extend([tuple(location3), tuple(location4)])
+        else:
+            location3_str = ["", "0", "0"]
+            location4_str = ["", "0", "0"]
 
         try:
             time_str = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -85,14 +82,6 @@ def index():
         except Exception as e:
             logging.exception("Error during map generation: %s", e)
             return str(e)
-        
-        logging.info("After map generation")
-
-        try:
-            logging.info(f"Checking if directory exists: {os.path.isdir('/home/zartyblartfast/GreatCircle_MapProjections')}")
-            logging.info(f"Checking write access to directory: {os.access('/home/zartyblartfast/GreatCircle_MapProjections', os.W_OK)}")
-        except Exception as e:
-            logging.error(f"Error during directory checks: {e}")
 
     return os.getcwd() + '<br>' + render_template('index.html', 
                                                   filename_plate_carree=filename_plate_carree, 
@@ -105,12 +94,12 @@ def index():
 
 @app.route('/map1/<filename>', methods=['GET'])
 def serve_map1(filename):
-    image_path = os.path.join('/home/zartyblartfast/', filename)  # Images are generated directly in /home/zartyblartfast/
+    image_path = os.path.join('/home/zartyblartfast/', filename)
     return send_file(image_path, mimetype='image/png')
 
 @app.route('/map2/<filename>', methods=['GET'])
 def serve_map2(filename):
-    image_path = os.path.join('/home/zartyblartfast/', filename)  # Images are generated directly in /home/zartyblartfast/
+    image_path = os.path.join('/home/zartyblartfast/', filename)
     return send_file(image_path, mimetype='image/png')
 
 if __name__ == "__main__":
