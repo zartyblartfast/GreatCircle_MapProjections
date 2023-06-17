9bd1d0638f86af0394536ad96a4054766a8b715c
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-17 14:53:49 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,22 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     var spinner = document.getElementById('spinner');
     var secondPair = document.getElementById('secondPair');
-    var includeSecondPair = document.getElementById('includeSecondPair');
     var dropdown1 = document.getElementById('dropdown1');
     var dropdown2 = document.getElementById('dropdown2');
 
     spinner.style.display = "none"; // Hide the spinner initially
-    includeSecondPair.checked = true; // Check the checkbox initially
-
-    // Always show the second pair on the initial load
-    secondPair.style.display = "";
-
-    // Retrieve checkbox state from local storage and update checkbox state
-    var isChecked = localStorage.getItem('includeSecondPair');
-    if (isChecked === 'unchecked') {
-        includeSecondPair.checked = false;
-        secondPair.style.display = "none";
-    }
 
     // Fetch the locations.json file
     fetch('/static/locations.json')
@@ -66,25 +54,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     document.getElementById('locationsForm').addEventListener('submit', function(e) {
         spinner.style.display = 'inline';
-        // Save checkbox state
-        localStorage.setItem('includeSecondPair', includeSecondPair.checked ? 'checked' : 'unchecked');
-    });
-
-    includeSecondPair.addEventListener('change', function() {
-        if (this.checked) {
-            // Show second pair and enable all inputs
-            secondPair.style.display = "";
-            var inputs = secondPair.querySelectorAll('input');
-            inputs.forEach(function(input) {
-                input.disabled = false;
-            });
-        } else {
-            // Hide second pair and disable all inputs
-            secondPair.style.display = "none";
-            var inputs = secondPair.querySelectorAll('input');
-            inputs.forEach(function(input) {
-                input.disabled = true;
-            });
-        }
     });
 });
