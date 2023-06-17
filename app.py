4bec61196a2d53dd2704320518d4c05ea19e241f
zartyblartfast <zarty_blartfast@yahoo.com>
2023-06-17 14:14:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,21 +11,17 @@ logging.basicConfig(filename='/home/zartyblartfast/GreatCircle_MapProjections/ap
 app = Flask(__name__)
 
 def convert_coord(coord_str):
-    if coord_str and len(coord_str) > 0:
-        try:
-            if coord_str[-1].upper() in ('N', 'S', 'E', 'W'):
-                direction = coord_str[-1].upper()
-                coord = float(coord_str[:-1])
-                if direction in ('S', 'W'):
-                    coord *= -1  # Switch to negative for S and W
-            else:
-                coord = float(coord_str)
-        except ValueError:
-            return None
-        return coord
-    else:
+    try:
+        if coord_str[-1].upper() in ('N', 'S', 'E', 'W'):
+            direction = coord_str[-1].upper()
+            coord = float(coord_str[:-1])
+            if direction in ('S', 'W'):
+                coord *= -1  # Switch to negative for S and W
+        else:
+            coord = float(coord_str)
+    except ValueError:
         return None
-
+    return coord
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -37,28 +33,39 @@ def index():
     location3_str = ["", "", ""]
     location4_str = ["", "", ""]
 
+    plot_second_pair = False
+
     if request.method == 'POST':
         logging.info('Received POST request')
 
         location1_str = [request.form.get('location1Name'), request.form.get('location1Lat'), request.form.get('location1Lon')]
         location2_str = [request.form.get('location2Name'), request.form.get('location2Lat'), request.form.get('location2Lon')]
-        location3_str = [request.form.get('location3Name'), request.form.get('location3Lat'), request.form.get('location3Lon')]
-        location4_str = [request.form.get('location4Name'), request.form.get('location4Lat'), request.form.get('location4Lon')]
+        plot_second_pair = 'plotSecondPair' in request.form
         
         location1 = [location1_str[0], convert_coord(location1_str[1]), convert_coord(location1_str[2])]
         location2 = [location2_str[0], convert_coord(location2_str[1]), convert_coord(location2_str[2])]
-        location3 = [location3_str[0], convert_coord(location3_str[1]), convert_coord(location3_str[2])]
-        location4 = [location4_str[0], convert_coord(location4_str[1]), convert_coord(location4_str[2])]
 
         if None in location1 or None in location2 or "" in location1_str or "" in location2_str:
             logging.error("Location 1 or Location 2 were not provided in correct format")
             return "Location 1 or Location 2 were not provided in correct format"
 
-        if None in location3 or None in location4 or "" in location3_str or "" in location4_str:
-            logging.error("Location 3 or Location 4 were not provided in correct format")
-            return "Location 3 or Location 4 were not provided in correct format"
+        locations = [tuple(location1), tuple(location2)]
 
-        locations = [tuple(location1), tuple(location2), tuple(location3), tuple(location4)]
+        if plot_second_pair:
+            location3_str = [request.form.get('location3Name'), request.form.get('location3Lat'), request.form.get('location3Lon')]
+            location4_str = [request.form.get('location4Name'), request.form.get('location4Lat'), request.form.get('location4Lon')]
+            
+            location3 = [location3_str[0], convert_coord(location3_str[1]), convert_coord(location3_str[2])]
+            location4 = [location4_str[0], convert_coord(location4_str[1]), convert_coord(location4_str[2])]
+
+            if None in location3 or None in location4 or "" in location3_str or "" in location4_str:
+                logging.error("Location 3 or Location 4 were not provided in correct format")
+                return "Location 3 or Location 4 were not provided in correct format"
+            
+            locations.extend([tuple(location3), tuple(location4)])
+        else:
+            location3_str = ["", "", ""]
+            location4_str = ["", "", ""]
 
         try:
             time_str = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -82,7 +89,8 @@ def index():
                                                   location1=location1_str,
                                                   location2=location2_str,
                                                   location3=location3_str,
-                                                  location4=location4_str)
+                                                  location4=location4_str,
+                                                  plot_second_pair=plot_second_pair)
 
 @app.route('/map1/<filename>', methods=['GET'])
 def serve_map1(filename):
@@ -95,4 +103,4 @@ def serve_map2(filename):
     return send_file(image_path, mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
